5dbc9c078b25f19077fa8805f2e303bee4b1bb05
SANN1TAR <artemis9774860374@gmail.com>
2026-04-30 15:50:10 +0300

    Поправлено приветствие

--- bot.py
+++ bot.py
@@ -92,7 +92,7 @@ async def cmd_start(message: Message, state: FSMContext):
     else:
         await message.answer(DISCLAIMER, parse_mode="HTML")
         await message.answer(
-            "Здорово, пта. Я тут из рода экскурсоводов — знаю почти все дыры в городе.\n\n"
+            "Здорово, ёпта. Я тут из рода экскурсоводов — знаю почти все дыры в городе.\n\n"
             "Может, тебе понадобятся крыши. Может, заброшки. А может вообще из этих — диггеры, подвалы всякие.\n\n"
             "Ты скажи как тебя кличут, а я пока подыщу что тебе понадобится.",
             reply_markup=ReplyKeyboardRemove(),
